c6304135f064c25a589ccd429197f93eefd9028e
Pablo <parusov.pashka@ya.ru>
2024-07-23 16:03:04 +0300

    fix model Task

--- internal/models/task.go
+++ internal/models/task.go
@@ -3,12 +3,12 @@ package models
 import "time"
 
 type Task struct {
-	ID          int           `json:"id"`
-	UserID      int           `json:"user_id"`
-	Description string        `json:"description"`
-	StartTime   time.Time     `json:"start_time"`
-	EndTime     time.Time     `json:"end_time"`
-	Duration    time.Duration `json:"duration"`
-	CreatedAt   time.Time     `json:"created_at"`
-	UpdatedAt   time.Time     `json:"updated_at"`
+	ID          int       `json:"id"`
+	UserID      int       `json:"user_id"`
+	Description string    `json:"description"`
+	StartTime   time.Time `json:"start_time"`
+	EndTime     time.Time `json:"end_time"`
+	Duration    string    `json:"duration"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
